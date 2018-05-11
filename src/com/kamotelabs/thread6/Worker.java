d9b255d2878ba7863f8c4025a9e02adbfc4f2cd5
Marlon <martianworm17@gmail.com>
2018-05-12 07:50:06 +0800

    Multiple Locks; Using Synchronized Code Blocks

--- src/com/kamotelabs/thread6/Worker.java
+++ src/com/kamotelabs/thread6/Worker.java
@@ -5,49 +5,70 @@ import java.util.List;
 import java.util.Random;
 
 public class Worker {
-	
+
 	private Random random = new Random();
-	
+
 	private List<Integer> list1 = new ArrayList<Integer>();
 	private List<Integer> list2 = new ArrayList<Integer>();
-	
-	public void stageOne() {
+
+	public synchronized void stageOne() {
 		try {
 			Thread.sleep(1);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		list1.add(random.nextInt(100));
 	}
-	
-	public void stageTwo() {
+
+	public synchronized void stageTwo() {
 		try {
 			Thread.sleep(1);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		list2.add(random.nextInt(100));
 	}
-	
+
 	public void process() {
-		for(int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			stageOne();
 			stageTwo();
 		}
 	}
 
-	
 	public void main() {
 		System.out.println("Starting ...");
-		
+
 		long start = System.currentTimeMillis();
+
+		Thread t1 = new Thread(new Runnable() {
+
+			public void run() {
+				process();
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			public void run() {
+				process();
+			}
+		});
+
+		t1.start();
+		t2.start();
 		
-		process();
-		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		long end = System.currentTimeMillis();
-		
+
 		System.out.println("Time take: " + (end - start));
 		System.out.println("List1: " + list1.size() + " List2: " + list2.size());
 	}
