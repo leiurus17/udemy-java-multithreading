ef77f117d102cc10052209c9e8fcc9f62402b15d
Marlon <martianworm17@gmail.com>
2018-05-12 12:04:25 +0800

    Wait and Notify

---
+++ src/com/kamotelabs/thread10/App.java
@@ -0,0 +1,9 @@
+package com.kamotelabs.thread10;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/kamotelabs/thread10/Processor.java
@@ -0,0 +1,13 @@
+package com.kamotelabs.thread10;
+
+public class Processor {
+	
+	public void produce() throws InterruptedException {
+		
+	}
+	
+	public void consume() throws InterruptedException {
+		
+	}
+
+}
